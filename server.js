a3a5387525e04c07108caaad84cab84454f378c3
stevey d <stphndcky22@gmail.com>
2022-04-09 05:47:55 -0700

    added Heroku, added query parameters

---
+++ server.js
@@ -0,0 +1,55 @@
+// requires express module
+const express = require('express');
+
+// requires data file
+const { animals } = require('./data/animals');
+
+// 'instantiates' the server
+const app = express();
+
+// create function for queries
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        // we save personality traits to array
+        // if query returns string, add it to array
+        // otherwise save array to variable 
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        }
+        else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        // create for loop
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter( animal => animal.personalityTraits.indexOf(trait) !== -1);
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+};
+
+// creates route we can request data from
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        // we call our query function within get method
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+// add listen method to express function
+// anonymous function console.logs message 
+app.listen(3001, () => {
+    console.log('API server now on port 3001, http://localhost:3001/api/animals?personalityTraits=hungry&personalityTraits=zany');
+});
